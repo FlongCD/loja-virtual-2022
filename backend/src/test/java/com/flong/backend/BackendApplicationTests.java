55ae2cfb7743165bdff1820a7c43791811b8b9f0
FlongCD <flongcd@gmail.com>
2022-12-24 15:43:25 -0300

    iniciando

---
+++ backend/src/test/java/com/flong/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flong.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
