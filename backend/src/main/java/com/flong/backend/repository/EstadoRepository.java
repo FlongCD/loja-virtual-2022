5a5dabca240923a480efaae33a8ebc9e4da7a336
FlongCD <flongcd@gmail.com>
2022-12-25 18:49:04 -0300

    crud estado

---
+++ backend/src/main/java/com/flong/backend/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.flong.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flong.backend.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
+    
+}
